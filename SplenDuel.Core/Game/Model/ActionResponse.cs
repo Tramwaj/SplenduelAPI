54de53c2f83784205d0462b1b398dd7bd6d7e63e
Magic <tramwajek@gmail.com>
2024-09-13 20:22:45 +0200

    extract ActionState class

--- SplenDuel.Core/Game/Model/ActionResponse.cs
+++ SplenDuel.Core/Game/Model/ActionResponse.cs
@@ -10,7 +10,7 @@ namespace Splenduel.Core.Game.Model
     internal class ActionResponse : DefaultResponse
     {        
         public List<object> ChangedObjects => (Object as List<object>)?? null;
-        public string State { get; set; }
+        public string State { get; set; } = ActionState.Normal;
         public ActionResponse(bool success, string? message = "") : base(success, message)
         {
         }
@@ -24,15 +24,5 @@ namespace Splenduel.Core.Game.Model
             State = state;
         }
         public static new ActionResponse Nok(string error) => new ActionResponse(false, error);
-    }
-    internal static class ActionState
-    {
-        public static string Normal = "Normal";
-        public static string EndTurn = "EndTurn";
-        public static string DropCoins = "DropCoins";
-        public static string ExchangeScroll = "ExchangeScroll";
-        public static string ReserveCard = "ReserveCard";
-        public static string StealCoin = "StealCoin";
-        public static string Pickup(ColourEnum colour) => "Pickup " + colour.ToString();
     }
 }
---
+++ SplenDuel.Core/Game/Model/ActionState.cs
@@ -0,0 +1,13 @@
+﻿namespace Splenduel.Core.Game.Model
+{
+    internal static class ActionState
+    {
+        public static string Normal = "Normal";
+        public static string EndTurn = "EndTurn";
+        public static string DropCoins = "DropCoins";
+        //public static string ExchangeScroll = "ExchangeScroll";
+        public static string ReserveCard = "ReserveCard";
+        public static string StealCoin = "StealCoin";
+        public static string Pickup(ColourEnum colour) => "Pickup " + colour.ToString();
+    }
+}
