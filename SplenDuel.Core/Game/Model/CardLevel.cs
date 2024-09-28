49ffd8209f9249e8ab9cf2045d0dbffc43ee3d3c
Magic <tramwajek@gmail.com>
2024-09-28 18:09:26 +0200

    return empty card not null if drawn from empty deck

--- SplenDuel.Core/Game/Model/CardLevel.cs
+++ SplenDuel.Core/Game/Model/CardLevel.cs
@@ -44,7 +44,7 @@ namespace Splenduel.Core.Game.Model
         //todo: tests
         public Card? DrawCardFromDeck()
         {
-            if (_deck.Count == 0) return null;
+            if (_deck.Count == 0) return new Card();
             var cardDrawn = _deck.ElementAt(_random.Next(_deck.Count));
             _deck.Remove(cardDrawn);
             return cardDrawn;
