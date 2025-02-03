5591832a4acfe07569c943a7930ba777ac96bd8f
Magic <tramwajek@gmail.com>
2025-02-03 14:12:40 +0100

    give every card a random action

--- SplenDuel.Core/Game/Services/GameCreator.cs
+++ SplenDuel.Core/Game/Services/GameCreator.cs
@@ -48,23 +48,44 @@ namespace Splenduel.Core.Game.Services
                 throw;
             }
         }
+        private Card GiveCardRandomAction(Card card)
+        {
+            var random = new Random();
+            var action = (CardActionEnum)random.Next(0, 4);
+            card.Action = action;
+            return card;
+        }
 
         private async Task<Board> CreateBoard()
         {
             try
             {
                 var lvl1Cards = (await _gameStore.GetAllCardsCsvFromLevel(1)).Select((x, i) => CardFromCsvReader.Read(x, i, 1)).ToList();
+                    //.Select(c => GiveCardRandomAction(c)).ToList();
                 var cardLevel1 = new CardLevel(lvl1Cards, 5);
-                var lvl2Cards = (await _gameStore.GetAllCardsCsvFromLevel(2)).Select((x, i) => CardFromCsvReader.Read(x, i+100, 2)).ToList();
+                var lvl2Cards = (await _gameStore.GetAllCardsCsvFromLevel(2)).Select((x, i) => CardFromCsvReader.Read(x, i + 100, 2)).ToList();
                 var cardLevel2 = new CardLevel(lvl2Cards, 4);
-                var lvl3Cards = (await _gameStore.GetAllCardsCsvFromLevel(3)).Select((x, i) => CardFromCsvReader.Read(x, i+200, 3)).ToList();
+                var lvl3Cards = (await _gameStore.GetAllCardsCsvFromLevel(3)).Select((x, i) => CardFromCsvReader.Read(x, i + 200, 3)).ToList();
                 var cardLevel3 = new CardLevel(lvl3Cards, 3);
                 var coinBoard = new CoinBoard(2);
                 var player1Board = new PlayerBoard(false);
+                var cost = new CardCost(0, 0, 0, 0, 0, 0);
+                //await player1Board.BuyCard(new Card(500, 3, ColourEnum.Blue, 0, 3, 0, cost, CardActionEnum.None));
+                //await player1Board.BuyCard(new Card(500, 3, ColourEnum.White, 0, 3, 0, cost, CardActionEnum.None));
+                //await player1Board.BuyCard(new Card(500, 3, ColourEnum.Red, 0, 3, 0, cost, CardActionEnum.None));
+                //await player1Board.BuyCard(new Card(500, 3, ColourEnum.Black, 0, 3, 0, cost, CardActionEnum.None));
+                //await player1Board.BuyCard(new Card(500, 3, ColourEnum.Green, 0, 3, 0, cost, CardActionEnum.None));
+                //await player1Board.AddCoins(new List<ColourEnum> { ColourEnum.Pink, ColourEnum.Pink });
                 var player2Board = new PlayerBoard(true);
-                Noble[] nobles = [new Noble(2, CardActionEnum.Scroll), 
-                    new Noble(2, CardActionEnum.ExtraTurn), 
-                    new Noble(2, CardActionEnum.Steal), 
+                //await player2Board.BuyCard(new Card(500, 3, ColourEnum.Blue, 0, 3, 0, cost, CardActionEnum.None));
+                //await player2Board.BuyCard(new Card(500, 3, ColourEnum.White, 0, 3, 0, cost, CardActionEnum.None));
+                //await player2Board.BuyCard(new Card(500, 3, ColourEnum.Red, 0, 3, 0, cost, CardActionEnum.None));
+                //await player2Board.BuyCard(new Card(500, 3, ColourEnum.Black, 0, 3, 0, cost, CardActionEnum.None));
+                //await player2Board.BuyCard(new Card(500, 3, ColourEnum.Green, 0, 3, 0, cost, CardActionEnum.None));
+                //await player2Board.AddCoins(new List<ColourEnum> { ColourEnum.Pink, ColourEnum.Pink });
+                Noble[] nobles = [new Noble(2, CardActionEnum.Scroll),
+                    new Noble(2, CardActionEnum.ExtraTurn),
+                    new Noble(2, CardActionEnum.Steal),
                     new Noble(3, CardActionEnum.None)];
                 return new Board(cardLevel1, cardLevel2, cardLevel3, coinBoard, player1Board, player2Board, nobles);
             }
